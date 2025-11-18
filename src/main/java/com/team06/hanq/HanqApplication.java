b2c63b23ddac0ec916474e1ebe611972cbed9ac9
jinha0907 <kjhakjha0101@gmail.com>
2025-11-18 23:07:18 +0900

    feat: add mockup project

---
+++ src/main/java/com/team06/hanq/HanqApplication.java
@@ -0,0 +1,13 @@
+package com.team06.hanq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team06/hanq/HanqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team06.hanq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
