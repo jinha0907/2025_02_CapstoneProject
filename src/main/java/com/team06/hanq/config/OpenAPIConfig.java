57a4e4795cebe6d72001ad83f9e9dba7f662ce15
jinha0907 <kjhakjha0101@gmail.com>
2025-11-29 17:31:54 +0900

    conf: swagger API title / version added

--- src/main/java/com/team06/hanq/config/OpenAPIConfig.java
+++ src/main/java/com/team06/hanq/config/OpenAPIConfig.java
@@ -5,8 +5,6 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.ObjectSchema;
 import io.swagger.v3.oas.models.media.Schema;
-import io.swagger.v3.oas.models.responses.ApiResponse;
-import io.swagger.v3.oas.models.responses.ApiResponses;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,20 +13,20 @@ public class OpenAPIConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        // ⚙️ ErrorResponse 모델을 Swagger에 등록
-        Components components = new Components()
-                .addSchemas("ErrorResponse", new ObjectSchema()
-                        .addProperty("code", new Schema<String>().example("USER_NOT_FOUND"))
-                        .addProperty("message", new Schema<String>().example("해당 유저를 찾을 수 없습니다."))
-                        .addProperty("status", new Schema<Integer>().example(404))
-                        .addProperty("timestamp", new Schema<String>().example("2025-11-28T18:50:31.245"))
-                );
+
+        ObjectSchema errorResponseSchema = new ObjectSchema();
+        errorResponseSchema.addProperty("code", new Schema<>().type("string").example("USER_NOT_FOUND"));
+        errorResponseSchema.addProperty("message", new Schema<>().type("string").example("해당 유저를 찾을 수 없습니다."));
+        errorResponseSchema.addProperty("status", new Schema<>().type("integer").example(404));
+        errorResponseSchema.addProperty("timestamp", new Schema<>().type("string").example("2025-11-28T18:50:31.245"));
+
+        Components components = new Components().addSchemas("ErrorResponse", errorResponseSchema);
 
         return new OpenAPI()
+                .components(components)
                 .info(new Info()
                         .title("HANQ API")
-                        .version("v1.0")
-                        .description("한국 문화 학습 및 퀴즈 서비스 백엔드 API"))
-                .components(components);
+                        .version("v1.0.0")
+                        .description("한국 문화 학습 및 퀴즈 서비스 백엔드 API 명세서"));
     }
 }
